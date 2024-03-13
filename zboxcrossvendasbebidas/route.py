df3817a04edc35325a390ab7b7887a510bf08e38
bryan <bryan.gps@gmail.com>
2024-03-13 10:31:02 -0300

    colocado, a exibição de mensagem flash dentro das paginas

--- zboxcrossvendasbebidas/route.py
+++ zboxcrossvendasbebidas/route.py
@@ -1,5 +1,5 @@
 from zboxcrossvendasbebidas import app
-from flask import redirect, request, render_template
+from flask import redirect, request, render_template, flash, url_for
 from zboxcrossvendasbebidas.forms import FormLogin, FormCriarCadastro
 
 
@@ -8,16 +8,21 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
-
+    if form_login.validate_on_submit():
+        flash('Login feito com sucesso', 'alert-success')
+        return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login)
 
 
-@app.route('/criar_conta')
+@app.route('/criar_conta', methods=['GET', 'POST'])
 def criar_cadastro():
     form_criarcadastro = FormCriarCadastro()
+    if form_criarcadastro.validate_on_submit():
+        flash('Conta criada com sucesso', 'alert-success')
+        return redirect(url_for('home'))
     return render_template('criarCadastro.html', form_criarcadastro=form_criarcadastro)
 
 
